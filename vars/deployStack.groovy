27fa3cdf386f2e926ffeaaa2dbf9da161cc05207
jasonluck <jluck@meetveracity.com>
2017-10-27 13:10:21 -0400

    escape $'s

--- vars/deployStack.groovy
+++ vars/deployStack.groovy
@@ -60,7 +60,7 @@ def call(body) {
         echo 'Containers are successfully deployed'
 
         def service = "${stackName}_${config.serviceName}"
-        publishedPort = sh(returnStdout: true, script: "docker --host ${config.dockerHost} service inspect ${service} --format '{{range $p, $conf := .Endpoint.Ports}} {{($conf).PublishedPort}} {{end}}'")
+        publishedPort = sh(returnStdout: true, script: "docker --host ${config.dockerHost} service inspect ${service} --format '{{range \$p, \$conf := .Endpoint.Ports}} {{(\$conf).PublishedPort}} {{end}}'")
     }
 
     return publishedPort
