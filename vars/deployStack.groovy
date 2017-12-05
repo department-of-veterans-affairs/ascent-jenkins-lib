8174dc93d79fdb3f3642cfb3d21e2fe79d48d63c
jasonluck <jluck@meetveracity.com>
2017-12-04 20:30:47 -0500

    Don't truncate deployment status

--- vars/deployStack.groovy
+++ vars/deployStack.groovy
@@ -72,7 +72,7 @@ def call(body) {
 
         echo 'Sleep for a few minutes and cross our fingers that the services started. Need to find a more reliable way of checking container health.'
         sleep(config.deployWaitTime)
-        sh "docker --host ${config.dockerHost} stack ps ${stackName}"
+        sh "docker --host ${config.dockerHost} stack ps ${stackName} --no-trunc"
         echo 'Containers are successfully deployed'
 
         def service = "${stackName}_${config.serviceName}"
