708960304ce2c65d5276232f82df024024014a55
jasonluck <jluck@meetveracity.com>
2017-12-07 22:06:54 -0500

    fix move command

--- vars/mavenRelease.groovy
+++ vars/mavenRelease.groovy
@@ -69,7 +69,7 @@ def call(body) {
             sh "git branch temp"
             sh "git checkout temp"
             //Point the remote branch to the detached HEAD
-            sh "git branch –f ${BRANCH_NAME} temp"
+            sh "git branch –f -M ${BRANCH_NAME} temp"
             //Delete the temp branch
             sh "git branch –d temp"
             //Push the branch to the remote
