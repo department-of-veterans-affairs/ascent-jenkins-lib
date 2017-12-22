31c797f0f39c105f6b414fdb568c3c99e130a0af
jasonluck <jluck@meetveracity.com>
2017-12-22 13:45:50 -0500

    skip the master check

--- vars/mavenRelease.groovy
+++ vars/mavenRelease.groovy
@@ -14,15 +14,6 @@ def call(body) {
         def url = ''
         stage('Check master branch') {
            url = sh(returnStdout: true, script: 'git config remote.origin.url').trim()
-            sh "git fetch --no-tags --progress ${url} +refs/heads/master:refs/remotes/origin/master"
-
-            //Compare to master branch to look for any unmerged changes
-            def commitsBehind = sh(returnStdout: true, script: "git rev-list --right-only --count HEAD...remotes/origin/master").trim().toInteger()
-            if (commitsBehind > 0) {
-                error("Master Branch has changesets not included on this branch. Please merge master into your branch before releaseing.")
-            } else {
-                echo "Branch is up to date with changesets on master. Proceeding with release..."
-            }
 
             sh "git fetch origin"
             sh "git checkout ${env.BRANCH_NAME}"
