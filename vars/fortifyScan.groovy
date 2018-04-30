fe49f01343befbf4dc7847cac141c6a418a23f3c
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-04-30 11:31:49 -0500

    PlAT-182: rename the project_name variable

--- vars/fortifyScan.groovy
+++ vars/fortifyScan.groovy
@@ -9,16 +9,16 @@ def call(body) {
       config.directory = '.'
   }
 
-  if (config.project_name == null) {
+  if (config.projname == null) {
       def names = (config.directory.split("/"))
       // Get the last element (-1 wraps to the last element)
-      config.project_name = names[-1]
+      config.projname = names[-1]
   }
 
   node ('fortify-sca') {
     dir("${config.directory}") {
       stage('Fortify Analyzer') {
-          sh "sourceanalyzer -b ${config.project-name} -scan"
+          sh "sourceanalyzer -b ${config.projname} -scan"
       }
     }
   }
