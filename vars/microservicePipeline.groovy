830de288c137b43130559c09b097a47590dc2b13
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-04-30 13:52:20 -0500

    PLAT-182: have the microservice Pipeline call the fortifyScan, just as a test

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -53,6 +53,10 @@ def call(body) {
                     mavenSettings = config.mavenSettings
                 }
 
+                fortifyScan {
+                  directory = config.directory
+                }
+
                 echo "Build Result is: ${currentBuild.result}"
                 if (currentBuild.result == null) {
                     def builds = [:]
