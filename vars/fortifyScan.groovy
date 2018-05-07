9b976965eff1193d8e5cb4dd105ed07a1c8ff020
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-05-07 11:12:03 -0500

    PLAT-182: make the file path a url

--- vars/fortifyScan.groovy
+++ vars/fortifyScan.groovy
@@ -53,7 +53,7 @@ def call(body) {
           // -- Check if a fortifyScan was generated, and if it was, then use the report generator to convert
           //    it to a pdf
           def currDir = pwd(tmp: false)
-          def fprFile = new File("${currDir}/${fortifyScanResults}")
+          def fprFile = new File("file:/${currDir}/${fortifyScanResults}")
           if(fprFile.exists()) {
             sh "ReportGenerator -format pdf -f target/fortify-${config.projname}-scan.pdf -source target/fortify-${config.projname}-scan.fpr"
             archive "target/fortify-${config.projname}-scan.xml"
