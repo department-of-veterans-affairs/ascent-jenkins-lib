b2dc29262d2655045acaaa0d124028ea18674b85
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-11-06 09:56:24 -0600

    add a rollback, deploy from tag instead of master, a check if there are conflicts if merged and fail if there are any

--- vars/deployProd.groovy
+++ vars/deployProd.groovy
@@ -41,45 +41,25 @@ def call(body) {
   def url = ''
   def urlMinusProtocol = ''
   withCredentials([usernamePassword(credentialsId: 'github', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-      stage('Check master branch') {
-          url = sh(returnStdout: true, script: 'git config remote.origin.url').trim()
-          urlMinusProtocol = url.substring(url.indexOf('://')+3)
-
-          sh "git fetch --no-tags --progress https://${GIT_USERNAME}:${GIT_PASSWORD}@${urlMinusProtocol} +refs/tags/${config.prodVersion}:refs/remotes/origin/master"
-
-          //Compare to master branch to look for any unmerged changes in the tag
-          def commitsBehind = sh(returnStdout: true, script: "git rev-list --right-only --count refs/tags/${config.prodVersion}...remotes/origin/master").trim().toInteger()
-          if (commitsBehind > 0) {
-              error("Master Branch has changesets not included in this tag ${config.prodVersion}. Please merge master into the tag before deploying to prod.")
-          } else {
-              echo "Tag ${config.prodVersion} is up to date with changesets on master. Proceeding with release..."
-          }
-
-          //Let's do the same thing with master too, just in case
-          def commitsBehind = sh(returnStdout: true, script: "git rev-list --right-only --count refs/heads/master...remotes/origin/master").trim().toInteger()
-          if (commitsBehind > 0) {
-              error("Master Branch has changesets not included in this branch. Please merge master into this branch before deploying to prod.")
-          } else {
-              echo "Branch is up to date with changesets on master. Proceeding with release..."
-          }
-
-          sh "git fetch https://${GIT_USERNAME}:${GIT_PASSWORD}@${urlMinusProtocol} +refs/tags/${config.prodVersion}:refs/tags/${config.prodVersion}"
-
-          // Merge tag into master
-          sh "git merge tags/${config.prodVersion}"
-      }
-
-      stage('Push to master branch'){
-        // Commit to master
-        sh "git commit -a -m \"Merge tag ${config.prodVersion} with master\""
-        //Push the release version to master branch
-        sh "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${urlMinusProtocol} master"
+      stage('Check for conflicts') {
+        // Get the tags from the origin repo
+          sh "git fetch --tags origin"
+        // Do a local merge without committing anything, checking for conflicts
+          def isConflict =  sh(returnStdout: true, script:"git merge --no-commit --no-ff tags/${config.prodVersion} | grep CONFLICT").matches("CONFLICT.*")
+        if(isConflict) {
+          // abort the merge
+          sh "git merge --abort"
+          error("Conflict between tag ${config.prodVersion} and master!"
+        }
       }
   }
 
   def keystoreAlias = "prod"
 
   stage('Deploy to Production') {
+    println("Checkout tags/${config.prodVersion}")
+    // Deploy from the tag
+    sh "git checkout tags/${config.prodVersion}"
     def prodEnvPort = deployStack {
       composeFiles = config.composeFiles
       stackName = config.stackName
@@ -101,30 +81,64 @@ def call(body) {
   }
 
   def prodHost = this.env.PROD_DOCKER_SWARM_MANAGER[6..-6]
+  def isValidDeploy = true
 
-  // --- Not really sure what tests to run on prod, but for now maybe functional and perf?
   stage('Validate') {
-    // mavenFunctionalTest {
-    //     directory = config.directory
-    //     serviceUrl = "${prodHost}:${prodEnvPort}"
-    //     cucumberReportDirectory = config.cucumberReportDirectory
-    //     testVaultTokenRole = config.testVaultTokenRole
-    //     cucumberOpts = config.cucumberOpts
-    //     options = config.intTestOptions
-    //     keystore = "${this.env.DOCKER_CERT_LOCATION}/${keystoreAlias}.jks"
-    //     keystorePassword = "changeit"
-    // }
-    //
-    // mavenPerformanceTest {
-    //     directory = config.directory
-    //     serviceProtocol = "https"
-    //     serviceHost = "${prodHost}"
-    //     servicePort = "${prodEnvPort}"
-    //     testVaultTokenRole = config.testVaultTokenRole
-    //     options = config.perfTestOptions
-    //     keystore = "${this.env.DOCKER_CERT_LOCATION}/${keystoreAlias}.jks"
-    //     keystorePassword = "changeit"
-    // }
+    // --- Some kind of smoke testing here, if they fail, flip isValidDeploy to false
   }
 
+
+
+  // Merge tag into master
+  if (isValidDeploy) {
+    stage ('Merge master') {
+      print("Merging tags/${config.prodVersion} to master...")
+      withCredentials([usernamePassword(credentialsId: 'github', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+        url = sh(returnStdout: true, script: 'git config remote.origin.url').trim()
+        urlMinusProtocol = url.substring(url.indexOf('://')+3)
+        sh "git checkout master"
+        def isNoMerge = sh(returnStdout: true, script:"git merge --no-commit --no-ff tags/${config.prodVersion}").matches("Already up to date.*")
+
+        // Abort the local changes if the tag is up to date with the master
+        if(isNoMerge) {
+          sh "git merge --abort"
+        } else {
+          // Go ahead and commit if the tag has changes
+          sh "git commit -a -m \"Merge tag ${config.prodVersion} into master\""
+          //Push the merged in tag to master branch
+          sh "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${urlMinusProtocol} master"
+        }
+      }
+    }
+  } else {
+    stage ("Rollback Deployment") {
+      //Undeploy from the tag
+      undeployStack {
+          dockerHost = "${this.env.PROD_DOCKER_SWARM_MANAGER}"
+          dockerDomain = this.env.DOCKER_PROD_DOMAIN
+          vaultAddr = "https://${this.env.PROD_VAULT_HOST}"
+          keystoreAlias = "${keystoreAlias}"
+      }
+      sh "git checkout master"
+      def prodEnvPort = deployStack {
+        composeFiles = config.composeFiles
+        stackName = config.stackName
+        serviceName = config.serviceName
+        vaultTokens = config.vaultTokens
+        deployWaitTime = config.deployWaitTime
+        keystoreAlias = "${keystoreAlias}"
+        dockerHost = this.env.PROD_DOCKER_SWARM_MANAGER
+        dockerDomain = this.env.DOCKER_PROD_DOMAIN
+        vaultAddr = "https://${this.env.PROD_VAULT_HOST}"
+        vaultCredID = "prodvault"
+        deployEnv = [
+          "SPRING_PROFILES_ACTIVE=aws-prod",
+          "RELEASE_VERSION=${this.prodVersion}",
+          "ES_HOST=${this.env.PROD_ES}",
+          "REPLICAS=3"
+        ]
+      }
+      error("Production deployment did not successfully pass smoke testing")
+    }
+  }
 }
